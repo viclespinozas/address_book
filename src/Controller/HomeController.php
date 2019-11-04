a1ff34cb0b6a3c72c1ade4a82045e103aafa3d53
Victor Espinoza <victorlespinozas@gmail.com>
2019-11-04 22:13:50 +0100

    added edit, remove and show functions as well as alert badges

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,14 +15,16 @@ class HomeController extends Controller
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
         $entityManager = $this->getDoctrine()->getManager();
         $contacts = $entityManager->getRepository(AddressBook::class)->findAll();
         $contactsData = $contacts ? $contacts : [];
+        $flag = $request->query->get('flag');
 
         return $this->render('index.html.twig', [
             'contactsData' => $contactsData,
+            'flag' => $flag
         ]);
     }
 
@@ -45,11 +47,83 @@ class HomeController extends Controller
             $entityManager->persist($contact);
             $entityManager->flush();
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('home', ['flag' => 'Contact created successfully!!']);
         }
 
         return $this->render('new_contact.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Matches /edit exactly
+     *
+     * @Route("/edit", name="edit_contact")
+     */
+    public function editContactAction(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $contactId = $request->query->get('contactId');
+        $contact = $entityManager->getRepository(AddressBook::class)->find($contactId);
+        $form = $this->createForm(AddressBookType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home', ['flag' => 'Contact updated successfully!!']);
+        }
+
+        return $this->render('edit_contact.html.twig', [
+            'form' => $form->createView(),
+            'contact' => $contact
+        ]);
+    }
+
+    /**
+     * Matches /show exactly
+     *
+     * @Route("/show", name="show_contact")
+     */
+    public function showContactAction(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $contactId = $request->query->get('contactId');
+        $contact = $entityManager->getRepository(AddressBook::class)->find($contactId);
+
+        $form = $this->createForm(AddressBookType::class, $contact);
+
+        $form->handleRequest($request);
+
+        return $this->render('show_contact.html.twig', [
+            'form' => $form->createView(),
+            'contact' => $contact
+        ]);
+    }
+
+    /**
+     * Matches /remove exactly
+     *
+     * @Route("/remove", name="remove_contact")
+     */
+    public function removeContactAction(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $contactId = $request->query->get('contactId');
+        $contact = $entityManager->getRepository(AddressBook::class)->find($contactId);
+
+        if ($request->isMethod("POST")) {
+            $entityManager->remove($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home', ['flag' => 'Contact removed successfully!!']);
+        }
+
+        return $this->render('remove_contact.html.twig', [
+            'contact' => $contact
         ]);
     }
 }
--- src/Form/AddressBookType.php
+++ src/Form/AddressBookType.php
@@ -5,6 +5,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\AddressBook;
 
 class AddressBookType extends AbstractType
 {
@@ -26,5 +28,12 @@ class AddressBookType extends AbstractType
             ])
             ->add('save', SubmitType::class)
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => AddressBook::class,
+        ]);
     }
 }
